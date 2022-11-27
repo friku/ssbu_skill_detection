3e29adf47345b9b8af9c526a45fe8189f27d8468
friku <riku.fujimoto@gmail.com>
2022-11-27 21:57:30 +0900

    labelの桁数を指定

--- detect.py
+++ detect.py
@@ -85,7 +85,7 @@ def detect(opt):
 
             p = Path(p)  # to Path
             save_path = str(save_dir / p.name)  # img.jpg
-            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame:010}')  # img.txt
             s += '%gx%g ' % img.shape[2:]  # print string
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             if len(det):
